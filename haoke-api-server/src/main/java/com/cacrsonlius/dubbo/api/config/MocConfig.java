259761b46425b6d2a31526cfa37d1fea643d88d1
liusen <liusen@huice.com>
2021-07-12 19:59:26 +0800

    mock

---
+++ haoke-api-server/src/main/java/com/cacrsonlius/dubbo/api/config/MocConfig.java
@@ -0,0 +1,22 @@
+package com.cacrsonlius.dubbo.api.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:mock-data.properties")
+@ConfigurationProperties("mock")
+@Data
+public class MocConfig {
+    private String indexMenu;
+    private String indexInfo;
+    private String indexFaq;
+    private String indexHouse;
+    private String infosList1;
+    private String infosList2;
+    private String infosList3;
+    private String my;
+
+}
---
+++ haoke-api-server/src/main/java/com/cacrsonlius/dubbo/api/controller/MockController.java
@@ -0,0 +1,67 @@
+package com.cacrsonlius.dubbo.api.controller;
+
+import com.alibaba.dubbo.common.json.JSONObject;
+import com.cacrsonlius.dubbo.api.config.MocConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectSerializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@RequestMapping("mock")
+@RestController
+@CrossOrigin
+public class MockController {
+    @Autowired
+    private MocConfig mocConfig;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping("indexMenu")
+    public Object indexMenu() throws IOException {
+        String indexMenu = mocConfig.getIndexMenu();
+        return objectMapper.readValue(indexMenu,objectMapper.getTypeFactory().constructType(HashMap.class));
+    }
+
+    @GetMapping("indexFaq")
+    public Object indexFaq(){
+        return mocConfig.getIndexFaq();
+    }
+
+    @GetMapping("indexHouse")
+    public Object indexHouse() throws IOException {
+        return objectMapper.readValue(mocConfig.getIndexHouse(),objectMapper.getTypeFactory().constructType(HashMap.class));
+    }
+
+    @GetMapping("indexInfo")
+    public Object indexInfo(){
+        return mocConfig.getIndexInfo();
+    }
+
+    @GetMapping("infosList1")
+    public Object infosList1(){
+        return mocConfig.getInfosList1();
+    }
+
+    @GetMapping("infosList2")
+    public Object infosList2(){
+        return mocConfig.getInfosList2();
+    }
+
+
+    @GetMapping("infosList3")
+    public Object infosList3(){
+        return mocConfig.getInfosList3();
+    }
+
+    @GetMapping("my")
+    public Object my(){
+        return mocConfig.getMy();
+    }
+}
